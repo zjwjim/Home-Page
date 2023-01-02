ee5d2efcd58649e049a943fbed1b0d992f79e4f9
晚秋 <zjwjim2007@163.com>
2023-01-02 18:22:00 +0800

    计时器

--- js/main.js
+++ js/main.js
@@ -14,6 +14,44 @@ function nav(id) {
 function blog() {
     window.open('https://www.wanqiu.tk')
 }
+function siteTime() {
+    var seconds = 1000;
+    var minutes = seconds * 60;
+    var hours = minutes * 60;
+    var days = hours * 24;
+    var years = days * 365;
+    var today = new Date();
+    var startYear = "2022";
+    var startMonth = "2";
+    var startDate = "4";
+    var startHour = "0";
+    var startMinute = "0";
+    var startSecond = "0";
+    var todayYear = today.getFullYear();
+    var todayMonth = today.getMonth() + 1;
+    var todayDate = today.getDate();
+    var todayHour = today.getHours();
+    var todayMinute = today.getMinutes();
+    var todaySecond = today.getSeconds();
+    var t1 = Date.UTC(startYear, startMonth, startDate, startHour, startMinute, startSecond);
+    var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
+    var diff = t2 - t1;
+    var diffYears = Math.floor(diff / years);
+    var diffDays = Math.floor((diff / days) - diffYears * 365);
+    var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
+    var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
+    var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds);
+
+    // 区分是否有年份.
+    if (diffYears > 0) {
+        var yearsAndDaysTip = '本站已安全运行 ' + diffYears + ' 年 ' + diffDays + ' 天 ' + diffHours + ' 小时 ' + diffMinutes + ' 分钟 ' + diffSeconds + ' 秒';
+        document.getElementById("sitetime").innerHTML = yearsAndDaysTip;
+    } else {
+        var daysTip = '本站已安全运行 ' + diffDays + ' 天 ' + diffHours + ' 小时 ' + diffMinutes + ' 分钟 ' + diffSeconds + ' 秒';
+        document.getElementById("sitetime").innerHTML = daysTip;
+    }
+}
+setInterval(siteTime, 1000);
 
 // 控制台输出
 console.log(String.raw`
